d2d6cbcc34400bc90803e0e8c8f2e815603fdcd6
James Hotchkin <jhotch@users.noreply.github.com>
2015-01-15 21:06:12 -0600

    fixing rainbow delims after updates

---
+++ elpa/epl-20140823.609/epl-autoloads.el
@@ -0,0 +1,18 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("epl-pkg.el" "epl.el") (21688 28177 479986
+;;;;;;  341000))
+
+;;;***
+
+(provide 'epl-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; epl-autoloads.el ends here
